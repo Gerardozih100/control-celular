9db6fea74f1944a4c05d15df5dd60507f64fde93
Gerardozih100 <106816077+Gerardozih100@users.noreply.github.com>
2022-06-12 19:09:13 -0500

    Falta Afinar pero parece que ya esta

--- main.ts
+++ main.ts
@@ -16,14 +16,6 @@ bluetooth.onBluetoothDisconnected(function () {
         # . . . #
         `)
 })
-input.onButtonPressed(Button.A, function () {
-    if (Posicion == 181) {
-        Posicion = 0
-    } else {
-        Posicion += 0 + 10
-    }
-    robotbit.Servo(robotbit.Servos.S1, Posicion)
-})
 input.onButtonPressed(Button.AB, function () {
     if (PIN == 3) {
         PIN = 0
@@ -31,66 +23,58 @@ input.onButtonPressed(Button.AB, function () {
         PIN += 0 + 1
     }
 })
-input.onButtonPressed(Button.B, function () {
-    if (Posicion == -1) {
-        Posicion = 180
-    } else {
-        Posicion += 0 - 10
-    }
-    robotbit.Servo(robotbit.Servos.S1, Posicion)
-})
 control.onEvent(EventBusSource.MES_DPAD_CONTROLLER_ID, EventBusValue.MICROBIT_EVT_ANY, function () {
     if (control.eventValue() == EventBusValue.MES_DPAD_BUTTON_1_DOWN) {
         basic.showArrow(ArrowNames.North)
         if (SERVO == 1) {
-            if (Posicion == 181) {
-                Posicion = 0
+            if (Posicion2 == 181) {
+                Posicion2 = 0
             } else {
-                Posicion += 0 + 10
-                robotbit.Servo(robotbit.Servos.S2, Posicion)
+                Posicion2 += 0 + 10
+                robotbit.Servo(robotbit.Servos.S2, Posicion2)
             }
         }
         if (SERVO == 2) {
-            if (Posicion == -1) {
-                Posicion = 180
+            if (Posicion3 == -1) {
+                Posicion3 = 180
             } else {
-                Posicion += 0 - 10
-                robotbit.Servo(robotbit.Servos.S3, Posicion)
+                Posicion3 += 0 - 10
+                robotbit.Servo(robotbit.Servos.S3, Posicion3)
             }
         }
     } else if (control.eventValue() == EventBusValue.MES_DPAD_BUTTON_2_DOWN) {
         basic.showArrow(ArrowNames.South)
         if (SERVO == 1) {
-            if (Posicion == -1) {
-                Posicion = 180
+            if (Posicion2 == -1) {
+                Posicion2 = 180
             } else {
-                Posicion += 0 - 10
-                robotbit.Servo(robotbit.Servos.S2, Posicion)
+                Posicion2 += 0 - 10
+                robotbit.Servo(robotbit.Servos.S2, Posicion2)
             }
         }
         if (SERVO == 2) {
-            if (Posicion == 181) {
-                Posicion = 0
+            if (Posicion3 == 181) {
+                Posicion3 = 0
             } else {
-                Posicion += 0 + 10
-                robotbit.Servo(robotbit.Servos.S3, Posicion)
+                Posicion3 += 0 + 10
+                robotbit.Servo(robotbit.Servos.S3, Posicion3)
             }
         }
     } else if (control.eventValue() == EventBusValue.MES_DPAD_BUTTON_3_DOWN) {
         basic.showArrow(ArrowNames.West)
-        if (Posicion == 181) {
-            Posicion = 0
+        if (Posicion1 == 181) {
+            Posicion1 = 0
         } else {
-            Posicion += 0 + 10
-            robotbit.Servo(robotbit.Servos.S1, Posicion)
+            Posicion1 += 0 + 10
+            robotbit.Servo(robotbit.Servos.S1, Posicion1)
         }
     } else if (control.eventValue() == EventBusValue.MES_DPAD_BUTTON_4_DOWN) {
         basic.showArrow(ArrowNames.East)
-        if (Posicion == -1) {
-            Posicion = 180
+        if (Posicion1 == -1) {
+            Posicion1 = 180
         } else {
-            Posicion += 0 - 10
-            robotbit.Servo(robotbit.Servos.S1, Posicion)
+            Posicion1 += 0 - 10
+            robotbit.Servo(robotbit.Servos.S1, Posicion1)
         }
     } else if (control.eventValue() == EventBusValue.MES_DPAD_BUTTON_A_DOWN) {
         basic.showString("A")
@@ -107,12 +91,11 @@ control.onEvent(EventBusSource.MES_DPAD_CONTROLLER_ID, EventBusValue.MICROBIT_EV
             . # . # .
             . . # . .
             `)
-        SERVO = 3
-        if (Posicion == 181) {
-            Posicion = 0
+        if (Posicion4 == -1) {
+            Posicion4 = 0
         } else {
-            Posicion += 0 + 180
-            robotbit.Servo(robotbit.Servos.S4, Posicion)
+            Posicion4 += 0 - 105
+            robotbit.Servo(robotbit.Servos.S4, Posicion4)
         }
     } else if (control.eventValue() == EventBusValue.MES_DPAD_BUTTON_D_DOWN) {
         basic.showString("D")
@@ -123,61 +106,67 @@ control.onEvent(EventBusSource.MES_DPAD_CONTROLLER_ID, EventBusValue.MICROBIT_EV
             . . # . .
             . . # . .
             `)
-        if (Posicion == -1) {
-            Posicion = 180
+        if (Posicion4 == 91) {
+            Posicion4 = 0
         } else {
-            Posicion += 0 - 180
-            robotbit.Servo(robotbit.Servos.S4, Posicion)
+            Posicion4 += 0 + 120
+            robotbit.Servo(robotbit.Servos.S4, Posicion4)
         }
     }
 })
+let Posicion4 = 0
+let Posicion3 = 0
+let Posicion2 = 0
 let PIN = 0
 let SERVO = 0
-let Posicion = 0
-Posicion = 90
+let Posicion1 = 0
+Posicion1 = 90
 SERVO = 0
 PIN = 0
-robotbit.Servo(robotbit.Servos.S1, Posicion)
-robotbit.Servo(robotbit.Servos.S2, Posicion)
-robotbit.Servo(robotbit.Servos.S3, Posicion)
-robotbit.Servo(robotbit.Servos.S4, Posicion)
+Posicion2 = 90
+Posicion3 = 90
+Posicion4 = 90
+robotbit.Servo(robotbit.Servos.S1, Posicion1)
+robotbit.Servo(robotbit.Servos.S2, Posicion2)
+robotbit.Servo(robotbit.Servos.S3, Posicion3)
+robotbit.Servo(robotbit.Servos.S4, Posicion4)
 basic.forever(function () {
     if (input.buttonIsPressed(Button.B)) {
         if (PIN == 0) {
-            Posicion += -2
-            robotbit.Servo(robotbit.Servos.S1, Posicion)
+            Posicion1 += -2
+            robotbit.Servo(robotbit.Servos.S1, Posicion1)
         }
         if (PIN == 1) {
-            Posicion += -2
-            robotbit.Servo(robotbit.Servos.S2, Posicion)
+            Posicion1 += -2
+            robotbit.Servo(robotbit.Servos.S2, Posicion1)
         }
         if (PIN == 2) {
-            Posicion += -2
-            robotbit.Servo(robotbit.Servos.S3, Posicion)
+            Posicion1 += -2
+            robotbit.Servo(robotbit.Servos.S3, Posicion1)
         }
         if (PIN == 3) {
-            Posicion += -2
-            robotbit.Servo(robotbit.Servos.S4, Posicion)
+            Posicion1 += -2
+            robotbit.Servo(robotbit.Servos.S4, Posicion1)
         }
     }
 })
 basic.forever(function () {
     if (input.buttonIsPressed(Button.A)) {
         if (PIN == 0) {
-            Posicion += 2
-            robotbit.Servo(robotbit.Servos.S1, Posicion)
+            Posicion1 += 2
+            robotbit.Servo(robotbit.Servos.S1, Posicion1)
         }
         if (PIN == 1) {
-            Posicion += 2
-            robotbit.Servo(robotbit.Servos.S2, Posicion)
+            Posicion1 += 2
+            robotbit.Servo(robotbit.Servos.S2, Posicion1)
         }
         if (PIN == 2) {
-            Posicion += 2
-            robotbit.Servo(robotbit.Servos.S3, Posicion)
+            Posicion1 += 2
+            robotbit.Servo(robotbit.Servos.S3, Posicion1)
         }
         if (PIN == 3) {
-            Posicion += 2
-            robotbit.Servo(robotbit.Servos.S4, Posicion)
+            Posicion1 += 2
+            robotbit.Servo(robotbit.Servos.S4, Posicion1)
         }
     }
 })
